1c7ffd08c2140161251ddacb5830bc1f9d42a93b
Eugene Helland <eugenehelland@mail.com>
2021-12-02 18:10:43 -0700

    Added a blur effect that layers underneath the popup while popup is active. If any input field is left empty an incomplete book will not be created when "Add Book to Shelf" is clicked. Wrote a function that converts author name to initials to prevent text spillover when book width is too small.

--- scripts/functions.js
+++ scripts/functions.js
@@ -13,9 +13,44 @@ function clearForm() {
   pages.value = '';
 }
 
+// Returns true if any form-field is left empty.
+function emptyFields() {
+  if (title.value == '' || author.value == '' || pages.value == 0) {
+    return true;
+  }
+}
+
 function createBook(title, author, pages) {
   let newBook = new Book(title, author, pages);
   bookshelf.push(newBook);
+}
+
+function initials(author) {
+  let initialsArr = [];
+  initialsArr.push(author[0]);
+
+  for (let i = 0; i < author.length; i++) {
+    let char = author[i];
+
+    if (char == ' ') {
+      initialsArr.push(author[i + 1]);
+    }
+  }
+
+  // Return first initial if only one word present for author.
+  if (initialsArr.length == 1) {
+    return initialsArr[0];
+  }
+
+  let initials = '';
+
+  initials += initialsArr.slice(0, 2).join('.');
+
+  if (initialsArr.length > 2) {
+    initials += '.' + initialsArr[initialsArr.length - 1];
+  }
+
+  return initials;
 }
 
 function bookHtml(newBook, width) {
@@ -25,7 +60,7 @@ function bookHtml(newBook, width) {
   div.innerHTML = `
   <div class="book" style="width: ${width}px">
     <span class="title">${newBook.title}</span>
-    <span class="author">${newBook.author}</span>
+    <span class="author" style="max-width: ${width}px">${newBook.author}</span>
   </div>
   `;
 
@@ -35,6 +70,10 @@ function bookHtml(newBook, width) {
 function populateShelf() {
   let newBook = bookshelf[bookshelf.length - 1];
   width = Math.round(newBook.pages / 5);
+
+  if (newBook.author.length > 3) {
+    newBook.author = initials(newBook.author);
+  }
 
   // Add html to the "id='bookshelf" div.
   bookHtml(newBook, width);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const addBook = document.querySelector('.add-book');
 const popup = document.querySelector('.popup');
+const blur = document.querySelector('.blur');
 // Form inputs.
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
@@ -16,8 +17,9 @@ addBook.addEventListener('click', () => {
   // Clear form.
   clearForm();
 
-  // Popup form becomes visble.
+  // Popup form becomes visble + background blur.
   popup.classList.toggle('hide');
+  blur.classList.toggle('hide');
 
   // While popup is active "Add Book" button is not clickable.
   addBook.classList.toggle('no-click');
@@ -25,11 +27,17 @@ addBook.addEventListener('click', () => {
 
 // Add listener to button on popup form
 submitBook.addEventListener('click', () => {
+  // Block submission if form has an empty field.
+  if (emptyFields()) {
+    return;
+  }
+
   // Add books to shelf for "bookshelf" array.
   createBook(title.value, author.value, pages.value);
   populateShelf();
 
-  // Hide form after submitting book.
+  // Hide form after submitting book. Remove blur.
   popup.classList.toggle('hide');
+  blur.classList.toggle('hide');
   addBook.classList.toggle('no-click');
 });
